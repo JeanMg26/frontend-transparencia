5999c3d011b3873a19dae54b31df4c57db662fb6
JeanMg26 <jeanmg25@gmail.com>
2023-12-23 19:23:17 -0500

    :zap: add styles editor

--- src/pages/admin/publications/operation/OperationPublication.vue
+++ src/pages/admin/publications/operation/OperationPublication.vue
@@ -135,9 +135,40 @@
                       list: 'only-icons',
                       options: ['left', 'center', 'right', 'justify'],
                     },
+                    {
+                      label: $q.lang.editor.fontSize,
+                      icon: $q.iconSet.editor.fontSize,
+                      fixedLabel: true,
+                      fixedIcon: true,
+                      list: 'no-icons',
+                      options: [
+                        'size-1',
+                        'size-2',
+                        'size-3',
+                        'size-4',
+                        'size-5',
+                        'size-6',
+                        'size-7',
+                      ],
+                    },
+                    {
+                      label: $q.lang.editor.formatting,
+                      icon: $q.iconSet.editor.formatting,
+                      list: 'no-icons',
+                      options: [
+                        'p',
+                        'h1',
+                        'h2',
+                        'h3',
+                        'h4',
+                        'h5',
+                        'h6',
+                        'code',
+                      ],
+                    },
                   ],
                   ['bold', 'italic', 'strike', 'underline'],
-                  ['upload', 'save'],
+                  ['quote', 'unordered', 'ordered', 'outdent', 'indent'],
                 ]"
               />
               <span v-if="descBackError" class="q-label-error">
